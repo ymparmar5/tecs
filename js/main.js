521c7e04ec413100879122e262ce775b2022ce3f
ym parmar <141340770+ymparmar07@users.noreply.github.com>
2024-06-16 18:25:54 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,5 @@
 $(document).ready(function(){
 
-     $('.fa-bars').click(function(){
-        $(this).toggleClass('fa-times');
-        $('.navbar').toggleClass('nav-toggle');
-    });
-
-    $(window).on('load scroll',function(){
-        $('.fa-bars').removeClass('fa-times');
-        $('.navbar').removeClass('nav-toggle');
-
-        if($(window).scrollTop()>35)
-        {
-            $('.header').css({'background':'#002e5f','box-shadow':'0 .2rem .5rem rgba(0,0,0,.4)'});
-        }
-        else
-        {
-            $('.header').css({'background':'none','box-shadow':'none'});
-        }
-    });
-
     const counters = document.querySelectorAll('.counter');
     const speed = 120;
     counters.forEach(counter => {
